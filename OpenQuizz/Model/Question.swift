db1a0126ff0c801b181c56144d3cbd52f12bead4
hoalex <svt75@hotmail.com>
2018-12-03 18:58:04 +0100

    Game.class created and completed

---
+++ OpenQuizz/Model/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  OpenQuizz
+//
+//  Created by Alex on 03/12/2018.
+//  Copyright © 2018 Alexandre Holet. All rights reserved.
+//
+
+import Foundation
+
+public struct Question{
+    var title = ""
+    var isCorrect = false
+}
